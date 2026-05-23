c1544ba55de211d6b66057dfbb5a5c3db600c6a8
Nemirtingas <nemirtingas@noreply.com>
2026-05-24 00:26:51 +0200

    Added some send flags

--- include/XboxReliableNetwork/XboxReliableNetwork.h
+++ include/XboxReliableNetwork/XboxReliableNetwork.h
@@ -39,6 +39,20 @@ class XRNLink;
 class XRNOutgoingPacket;
 class XRNIncomingPacket;
 
+#define XRNM_SEND_FLAG_RELIABLE 0x01
+#define XRNM_SEND_FLAG_SEQUENTIAL 0x02
+#define XRNM_SEND_FLAG_0x04 0x04
+#define XRNM_SEND_FLAG_LAZY_ACK 0x08
+#define XRNM_SEND_FLAG_0x10 0x10
+#define XRNM_SEND_FLAG_0x20 0x20
+#define XRNM_SEND_FLAG_COALESCED 0x40
+
+#define XRNM_CHANNEL_TYPE_1 0x7FFFFFFF
+#define XRNM_CHANNEL_TYPE_2 0x7EFFFFFF
+
+#define XRNM_DEFAULT_CHANNEL(channelType, channelId) ((channelType) - (channelId))
+#define XRNM_USER_CHANNEL(channelType, channelId) (todo)
+
 #include "XRNNet.h"
 #include "XRNUtils.h"
 #include "XRNLog.h"
